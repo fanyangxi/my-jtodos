b3b76b7499eff53a919832f4253b246d91311a4d
fanyangxi <fanyangxi@live.cn>
2015-09-24 16:43:04 +0800

    init v1

---
+++ src/com/test/Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * Created by yxfan on 9/24/15.
+ * testing ...
+ */
+public class Main {
+
+    public static void main (String[] args){
+
+        System.out.printf("hi, v1");
+        System.out.println("args = " + "what happened !");
+    }
+}
