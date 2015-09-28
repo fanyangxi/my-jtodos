f24665e7ee624a32f4b8e23df3fac8e75295d84d
fanyangxi <fanyangxi@live.cn>
2015-09-28 18:46:21 +0800

    Added repos / services, impls

--- src/main/java/jtodos/repository/TodoItemRepository.java
+++ src/main/java/jtodos/repository/TodoItemRepository.java
@@ -1,8 +1,11 @@
 package jtodos.repository;
 
+import jtodos.domain.TodoItem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by yxfan on 9/28/15.
@@ -10,4 +13,10 @@ import javax.transaction.Transactional;
 
 @Transactional
 public interface TodoItemRepository extends CrudRepository<jtodos.domain.TodoItem, Long> {
+
+    @Query("select new jtodos.domain.TodoItem(t.name, t.content) "
+            + "from todoitems t"
+            + "where t.userid = ?1 "
+            + "order by t.name ASC")
+    public List<TodoItem> findUserSpecificTodoItems(long userId);
 }
