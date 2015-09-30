b6de76e0eec86fba2d29aedb28368a76bf284b1b
fanyangxi <fanyangxi@live.cn>
2015-09-30 14:07:00 +0800

    finished update / create form v1

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -6,27 +6,66 @@
 
 var app = angular.module('myapp.controllers', ['myapp.services']);
 
-app.controller('TodoitemListController', ['$scope', 'TodoitemsFactory', 'TodoitemFactory', '$location', '$window',
-    function ($scope, TodoitemsFactory, TodoitemFactory, $location, $window) {
+app.controller('TodoitemsController', ['$scope', 'TodoitemsFactory', 'TodoitemFactory', '$location', '$window', '$timeout',
+    function ($scope, TodoitemsFactory, TodoitemFactory, $location, $window, $timeout) {
 
-        // callback for ng-click 'edit':
-        $scope.edit = function (id, todoitem) {
+        // callback for ng-click 'create':
+        $scope.create = function () {
+            //TodoitemFactory.create(***);
+
+            if(!$scope.currentTodoitem.userid)
+            {
+                alert("Please enter userid");
+                return;
+            }
+
+            if(!$scope.currentTodoitem.name)
+            {
+                alert("Please enter todoitem name");
+                return;
+            }
+
+            $scope.currentTodoitem.userid = '2';
+            TodoitemsFactory.create($scope.currentTodoitem);
+
+            // $location.path('/todoitem-creation');
+            // todo: using timeout here may not be a good idea
+            $timeout(function() {
+                $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
+            }, 500);
+        };
+
+        // callback for ng-click 'editing':
+        $scope.editing = function (id, todoitem) {
+            // load item data to data-form
             // $location.path('/todoitem-detail/' + id);
-            TodoitemFactory.update(todoitem);
             // $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
+            $scope.currentTodoitem = todoitem;
+        };
+
+        // callback for ng-click 'update':
+        $scope.update = function (id) {
+            // alert('update');
+            // $location.path('/todoitem-detail/' + id);
+            // TodoitemFactory.update(todoitem);
+            TodoitemFactory.update($scope.currentTodoitem);
             $window.location.href = $window.location.href;
         };
 
         // callback for ng-click 'delete':
         $scope.delete = function (id) {
-            TodoitemFactory.delete({ userid: '2', id: id });
-            // $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
-            $window.location.href = $window.location.href;
-        };
-
-        // callback for ng-click 'create':
-        $scope.create = function () {
-            $location.path('/todoitem-creation');
+            var retVal = confirm("Do you want to delete item: (" + id + ")?");
+            if (retVal == true) {
+                // $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
+                // $window.location.href = $window.location.href;
+                TodoitemFactory.delete({ userid: '2', id: id });
+                $timeout(function() {
+                    $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
+                }, 500);
+                return true;
+            } else {
+                return false;
+            }
         };
 
         $scope.todoitems = TodoitemsFactory.query({ userid: '2' });
@@ -36,16 +75,6 @@ app.controller('TodoitemListController', ['$scope', 'TodoitemsFactory', 'Todoite
 app.controller('TodoitemDetailController', ['$scope', 'TodoitemsFactory', 'TodoitemFactory', '$location',
     function ($scope, TodoitemsFactory, TodoitemFactory, $location) {
 
-        // callback for ng-click 'edit':
-        $scope.updateItem = function (id) {
-            alert('edit');
-            $location.path('/todoitem-detail/' + id);
-        };
-
-        // callback for ng-click 'create':
-        $scope.createItem = function () {
-            $location.path('/todoitem-creation');
-        };
     }]
 );
 
